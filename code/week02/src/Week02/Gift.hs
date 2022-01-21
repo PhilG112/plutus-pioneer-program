d5637b637b875c304685ec9700fd2188167a81ff
Philg112 <pgrujovski@protonmail.com>
2022-01-21 23:19:49 +1100

    hmm gift

--- code/week02/src/Week02/Gift.hs
+++ code/week02/src/Week02/Gift.hs
@@ -1,40 +1,54 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Week02.Gift where
 
-import           Control.Monad       hiding (fmap)
-import           Data.Map            as Map
-import           Data.Text           (Text)
-import           Data.Void           (Void)
+import           Control.Monad        hiding (fmap)
+import           Data.Aeson           (ToJSON, FromJSON)
+import           Data.List.NonEmpty   (NonEmpty (..))
+import           Data.Map             as Map
+import           Data.Text            (pack, Text)
+import           GHC.Generics         (Generic)
+import           Ledger               hiding (singleton)
+import qualified Ledger.Constraints   as Constraints
+import qualified Ledger.Typed.Scripts as Scripts
+import           Ledger.Value         as Value
+import           Ledger.Ada           as Ada
+import           Playground.Contract  (IO, ensureKnownCurrencies, printSchemas, stage, printJson)
+import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types     (KnownCurrency (..))
 import           Plutus.Contract
-import           PlutusTx            (Data (..))
 import qualified PlutusTx
-import qualified PlutusTx.Builtins   as Builtins
-import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
-import           Ledger              hiding (singleton)
-import           Ledger.Constraints  as Constraints
-import qualified Ledger.Scripts      as Scripts
-import           Ledger.Ada          as Ada
-import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
-import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types    (KnownCurrency (..))
-import           Prelude             (IO, Semigroup (..), String)
-import           Text.Printf         (printf)
+import           PlutusTx.Prelude     hiding (unless)
+import qualified Prelude              as P
+import           Schema               (ToSchema)
+import           Text.Printf          (printf)
+import Data.String
+import qualified PlutusTx.Builtins as Builtins
+import Data.Void
 
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 {-# INLINABLE mkValidator #-}
+--             Datum          Redeemer       Context
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkValidator _ _ _ = ()
 
-validator :: Validator
+validator :: Scripts.Validator
 validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
 
 valHash :: Ledger.ValidatorHash
@@ -49,28 +63,29 @@ type GiftSchema =
 
 give :: AsContractError e => Integer -> Contract w s e ()
 give amount = do
+    -- Pay a certain amount to this script address, pass in the validator hash, datum, amount to pay
     let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount
-    ledgerTx <- submitTx tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    ledgerTx <- submitTx tx -- Submit the transaction
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx -- wait for confirmation of the transaction
     logInfo @String $ printf "made a gift of %d lovelace" amount
 
 grab :: forall w s e. AsContractError e => Contract w s e ()
 grab = do
-    utxos <- utxosAt scrAddress
-    let orefs   = fst <$> Map.toList utxos
-        lookups = Constraints.unspentOutputs utxos      <>
-                  Constraints.otherScript validator
-        tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs]
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    utxos <- utxosAt scrAddress -- Looks up all utxos sitting at an address
+    let orefs   = fst <$> Map.toList utxos --fmap fst (Map.toList utxos), gets all references to the utxos
+        lookups = Constraints.unspentOutputs utxos      <> -- lookups to construct the transaction, tell the wallet where to findd all the utxos
+                  Constraints.otherScript validator -- Inform the wallet about the validator
+        tx :: Constraints.TxConstraints Void Void
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer (Builtins.mkI 17) | oref <- orefs] -- Construct list of transactions based on some constraints, pretty simple list comprehension here
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx -- submit transaction
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx -- await confirmation
     logInfo @String $ "collected gifts"
 
 endpoints :: Contract () GiftSchema Text ()
-endpoints = awaitPromise (give' `select` grab') >> endpoints
+endpoints = awaitPromise (give' `select` grab') >> endpoints -- Offer the user a choice, either give or grab an recursively do it
   where
-    give' = endpoint @"give" give
-    grab' = endpoint @"grab" $ const grab
+    give' = endpoint @"give" give -- give endpoint, endpoint will block execution and wait for the user to provide an integer
+    grab' = endpoint @"grab" $ const grab -- 
 
 mkSchemaDefinitions ''GiftSchema
 
